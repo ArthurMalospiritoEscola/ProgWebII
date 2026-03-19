1fff9e36ab855b72a5b8e0d9cc2a4d6c536d66b1
ArthurMalospiritoEscola <arthur.escola.200608@gmail.com>
2026-03-19 18:03:48 -0400

    mudanças da aula e começo da criação da tela inicial

--- Biblioteca/Controllers/BibliotecaController.cs
+++ Biblioteca/Controllers/BibliotecaController.cs
@@ -9,7 +9,8 @@ public class BibliotecaController : Controller
     {
         List<Livro> l1 = new List<Livro> {
             new Livro(150,"Martin Lutero","Harry Potter","Ficção real",new DateOnly(2008,07,28)),
-            new Livro(255,"Mr.Computer","Hexadecimal","Computação",new DateOnly(2000,01,01)) 
+            new Livro(255,"Mr.Computer","Hexadecimal","Computação",new DateOnly(2000,01,01)),
+            new Livro(432,"Reinaldo Teixeira","Pingo o cavalo","Suspense",new DateOnly(2007,04,13))
         };
         
 
