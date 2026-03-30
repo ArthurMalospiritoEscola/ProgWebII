cb59019c7e8b106ce2168f5d46afc2dff5a4e405
Arthur Alvarenga Malospirito <arthur.escola.200608@gmail.com>
2026-03-29 21:24:19 -0400

    Adição das telas de Livro, Autor, Autores. Algumas alterações básicas nas telas já feitas anteriormente e integração dos Models de Livro com Model de Autor

--- Biblioteca/Models/Autor.cs
+++ Biblioteca/Models/Autor.cs
@@ -4,4 +4,9 @@ public class Autor
 {
     public int Id {get;set;}
     public string? Nome {get;set;}
+
+    public Autor(string Nome)
+    {
+        this.Nome= Nome;
+    }
 }
--- Biblioteca/Models/Livro.cs
+++ Biblioteca/Models/Livro.cs
@@ -1,16 +1,17 @@
+
 namespace Biblioteca.Models;
 
 public class Livro {
 
     public int Id {get;set;}
     public int NumPaginas {get;set;}
-    public string? Autor {get;set;}
+    public Autor Autor {get;set;}
     public string? Titulo {get;set;}
     public string? Genero {get;set;}
     public DateOnly DataPublicacao {get;set;}
 
 
-    public Livro(int numPaginas, string autor, string titulo, string genero, DateOnly dataPublicacao) {
+    public Livro(int numPaginas, Autor autor, string titulo, string genero, DateOnly dataPublicacao) {
         this.NumPaginas = numPaginas;
         this.Autor = autor;
         this.Titulo = titulo;
